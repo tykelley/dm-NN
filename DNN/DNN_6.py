d9b138250432e74911d3e280c71b81e6d11ea28a
Aaron Chau <aaron@dhcp-v043-114.mobile.uci.edu>
2018-01-25 21:29:24 -0800

    Barely working, need to fix standardization and neuron structure

--- DNN/DNN_6.py
+++ DNN/DNN_6.py
@@ -36,7 +36,7 @@ DATA:               Illustris-1 (filtered)
 """
 
 
-# All lables for the CSV containing filtered and randomized Illustris data
+# Exact same column / feature names as pre-processed Illustris_1 CSV
 COLUMNS = ['New_Index',
             'Pre_Random_Index',
             'SubhaloBHMass',
@@ -80,31 +80,33 @@ COLUMNS = ['New_Index',
             'SubhaloVelY',
             'SubhaloVelZ']
 
-FEATURES = ['SubhaloBHMass',                        ## 0.775 corr. with Mass
-            'SubhaloGasMetallicity',                ## 0.113 corr. with Mass
-            #'SubhaloGasMetallicitySfr',            # 0.108 corr. with Mass
-            #'SubhaloSFR',                          # 0.070 corr. with Mass
-            'SubhaloStarMetallicity',               ## 0.170 corr. with Mass
-            'SubhaloStellarPhotometricsMassInRad',  ## 0.746 corr. with Mass
-            #'SubhaloStellarPhotometricsRad',       # 0.380 corr. with Mass
-            #'SubhaloStellarPhotometricsU',         # -0.153 corr. with Mass
-            #'SubhaloStellarPhotometricsB',         # -0.161 corr. with Mass
-            #'SubhaloStellarPhotometricsV',         # -0.167 corr. with Mass
-            #'SubhaloStellarPhotometricsK',         # -0.172 corr. with Mass
-            #'SubhaloStellarPhotometricsg',         # -0.163 corr. with Mass
-            #'SubhaloStellarPhotometricsr',         # -0.169 corr. with Mass
-            #'SubhaloStellarPhotometricsi',         # -0.171 corr. with Mass
-            #'SubhaloStellarPhotometricsz',         # -0.172 corr. with Mass
-            'SubhaloVelX',                          ## -0.00168 corr. with Mass
-            'SubhaloVelY',                          ## 0.00215 corr. with Mass
-            'SubhaloVelZ'                           ## 0.00147 corr. with Mass
+# Column / feature names of inputs
+FEATURES = ['SubhaloBHMass',                            ## 0.775 corr. with Halo Mass
+            'SubhaloGasMetallicity',                    ## 0.113 corr. with Halo Mass
+            #'SubhaloGasMetallicitySfr',                ## 0.108 corr. with Halo Mass
+            #'SubhaloSFR',                              ## 0.070 corr. with Halo Mass
+            'SubhaloStarMetallicity',                   ## 0.170 corr. with Halo Mass
+            'SubhaloStellarPhotometricsMassInRad',      ## 0.746 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsRad',           ## 0.380 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsU',             ## -0.153 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsB',             ## -0.161 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsV',             ## -0.167 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsK',             ## -0.172 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsg',             ## -0.163 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsr',             ## -0.169 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsi',             ## -0.171 corr. with Halo Mass
+            #'SubhaloStellarPhotometricsz',             ## -0.172 corr. with Halo Mass
+            'SubhaloVelX',                              ## -0.00168 corr. with Halo Mass
+            'SubhaloVelY',                              ## 0.00215 corr. with Halo Mass
+            'SubhaloVelZ'                               ## 0.00147 corr. with Halo Mass
             ]
 
+# Column / feature name of output
 LABEL =     'SubhaloMass'
 
 
-# Custom pipeline for easy input feature manipulation, which
-# returns input columns and output column as TF constants
+# Custom pipeline for easy input feature manipulation,
+# returns input columns and output column as TensorFlow constants
 def input_fn(data_set):
     feature_cols = {k: tf.constant(data_set[k].values) for k in FEATURES}
     #labels = tf.cast(tf.constant(data_set[LABEL].values), tf.float32)
@@ -113,34 +115,36 @@ def input_fn(data_set):
 
 
 def main(unused_argv):
-    # Read CSV's without headers and white space in between items
+    # Read in Illustris pre-processed CSV's as dataframes, renaming columns to COLUMNS
     training_set = pd.read_csv("Illustris_1_Training.csv", skipinitialspace=True,
                                skiprows=1, names=COLUMNS)
     test_set = pd.read_csv("Illustris_1_Test.csv", skipinitialspace=True,
                            skiprows=1, names=COLUMNS)
 
+    """
+    # Standardize Training Set (see SciKit Learn), write into new dataframe
+    training_set_scaled = {j: preprocessing.StandardScaler().fit_transform(training_set[j].values)
+                            for j in FEATURES}
 
-
-    # Normalize specified input features, but not output
-    #mm = MinMaxScaler()
-
-
-    # Training set normalization
-    #training_set_scaled = {j: mm.fit_transform(training_set[j].values) for j in FEATURES}
-    training_set_scaled = {j: preprocessing.StandardScaler().fit_transform(training_set[j].values) for j in FEATURES}
-
-    training_set_scaled_df = pd.DataFrame(training_set_scaled, index = training_set.index, 							columns=training_set.columns)
+    training_set_scaled_df = pd.DataFrame(training_set_scaled,
+                                            index = training_set.index,
+                                            columns=training_set.columns)
 
     training_set_scaled_df['SubhaloMass'] = training_set['SubhaloMass'].values
 
 
-    # Test set normalization
-    #test_set_scaled = {j: mm.fit_transform(test_set[j].values) for j in FEATURES}
-    test_set_scaled = {j: preprocessing.StandardScaler().fit_transform(test_set[j].values) for j in FEATURES}
+    # Standardize Test Set (see SciKit Learn), write into new dataframe
+    test_set_scaled = {j: preprocessing.StandardScaler().fit_transform(test_set[j].values)
+                        for j in FEATURES}
 
-    test_set_scaled_df = pd.DataFrame(test_set_scaled, index = test_set.index, 						columns=test_set.columns)
+    test_set_scaled_df = pd.DataFrame(test_set_scaled,
+                                        index = test_set.index,
+                                        columns=test_set.columns)
 
     test_set_scaled_df['SubhaloMass'] = test_set['SubhaloMass'].values
+    """
+
+
 
 
 
@@ -148,67 +152,62 @@ def main(unused_argv):
     feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 
 
-    # Set up a 5 layer deep neural network with 18, 36, 72, 36, 18 units, respectively
+
+
+
+    # Set up a 4 layer deep neural network with 7, 6, 5, 4 units, respectively
     regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
-                                              hidden_units=[7,6,5,4],
-                                              model_dir="/home/aaron/MLprograms/DM/DNN #6/DNN #6 Model",
-                                              #config=tf.contrib.learn.RunConfig(
-							#save_checkpoints_steps=1000,
-							#save_checkpoints_secs=None,
-							#save_summary_steps=20
-							#),
-					      #optimizer=tf.train.ProximalAdagradOptimizer(
-      						#learning_rate=0.01,
-      						#l1_regularization_strength=0.001
-    						#),
-					      #optimizer=tf.train.FtrlOptimizer,
-                                              activation_fn=tf.nn.crelu
+                                                hidden_units=[7,7,7,7,7,7,7,7,7,7,7,6,5,4],
+                                                model_dir="/Users/aaron/Documents/Research/MLprograms/DM/dm-NN/DNN/Model",
+                                                  #config=tf.contrib.learn.RunConfig
+                                                        #save_checkpoints_steps=1000
+                                                        #save_checkpoints_secs=None
+                                                        #save_summary_steps=20,
+
+                                                #optimizer=tf.train.ProximalAdagradOptimizer,
+                                                #learning_rate=0.01,
+                                                #l1_regularization_strength=0.001,
+                                                #activation_fn=tf.nn.crelu
                                               )
-                       # good units for 7 feats: 7, 14, 28, 54, 28, 14, 7
 
 
-    # Train the NN over 10,000 steps on the Training Set
-    regressor.fit(input_fn=lambda: input_fn(training_set_scaled_df),
-                  steps = 2000)
+    # Train the neural network for 10,000 steps, using the
+    # pre-processed and standardized Training Set
+    regressor.fit(input_fn=lambda: input_fn(training_set),
+                    steps = 2000)
 
 
-    # Test the NN on the Test Set and print MSE + Pearson Correlation b/w predicted vs true
-    ev = regressor.evaluate(input_fn=lambda: input_fn(test_set_scaled_df),
-			    steps=1,
-			    #metrics={
-				#'Predicted vs True (TEST DATA) Pearson Correlation':
-					#MetricSpec(
-						#metric_fn=metric_ops.streaming_pearson_correlation,
-						#prediction_key="scores"
-						#)
-				     #}
-			   )
+    # Test the neural network on the pre-processed and standardized Test Set
+    # and calculate the Mean Squared Error
+    ev = regressor.evaluate(input_fn=lambda: input_fn(test_set),
+			                     steps=1
+                    			    #metrics={
+                    				#'Predicted vs True (TEST DATA) Pearson Correlation':
+                    					#MetricSpec(
+                    						#metric_fn=metric_ops.streaming_pearson_correlation,
+                    						#prediction_key="scores"
+                    						#)
+                    				     #}
+                            )
 
-
-    # Run MSE metric on Test Set again
     loss_score = ev["loss"]
 
 
-    # Print out MSE metric for Test Set
+    # Print the Mean Squared Error for Test Set
     print("(TEST DATA) Loss: {0:f}".format(loss_score))
 
 
-
-    # Make predictions, using Test Set (which is fine temporarily since it is unseen by NN)
-    y = regressor.predict_scores(input_fn=lambda: input_fn(test_set_scaled_df))
-
-
-    # Print Predictions Array through TF's preferred method
+    # Print the exact Halo Mass predictions from the Test Set
+    y = regressor.predict_scores(input_fn=lambda: input_fn(test_set))
     predictions = list(itertools.islice(y, 6000))
     print("Predictions: {}".format(str(predictions)))
 
-    # Make a new Pandas DF from Predictions Array in order to write to CSV
-    df = pd.DataFrame(np.array(list(predictions)))
-    print (df)
+    # Write Test Set Halo Mass predictions to a CSV
+    #df = pd.DataFrame(np.array(list(predictions)))
+    #print (df)
     #df.to_csv('predicted_halo_masses_smallNN_alt.csv')
 
-
-    # To call Tensorboard: 'tensorboard --logdir=/home/aaron/MLprograms/DM/DNN\ #6/DNN\ #6\ Model/'
+    # To call Tensorboard: 'tensorboard --logdir=...'
 
 
 if __name__ == "__main__":
